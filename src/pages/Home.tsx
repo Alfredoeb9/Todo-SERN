a000f4e8e8cf4eefe30a1b4b8b9dff79082a0416
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-11 13:48:50 -0700

    [addTask] updated addTask callback to take in email as part of taskStructure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,20 +4,26 @@ import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { post, posts } from "../redux/features/postsSlice";
 import { Uregentlevel, Categories } from "../lib/hardData";
 import AddTask from "../components/AddTask";
+import { useSendPost } from "../hooks/useSendPost";
 
 export default function Home() {
   const [task, setTask] = useState("");
+  const [title, setTitle] = useState("");
   const [urgentLevel, setUrgentLevel] = useState("");
   const [categoryOption, setCategoryOption] = useState("");
+  const { sendPost, error, isLoading } = useSendPost();
   const dispatch = useAppDispatch();
+  const getEmail = JSON.parse(localStorage.getItem("user")!);
 
   const addTask = useCallback(() => {
     const taskStructure = {
-      urgentLevel: urgentLevel.length === 0 ? Uregentlevel[0] : urgentLevel,
-      post: task,
+      email: getEmail,
+      priority: urgentLevel.length === 0 ? Uregentlevel[0] : urgentLevel,
+      task,
       category: categoryOption.length === 0 ? Categories[0] : categoryOption,
+      title,
     };
-
+    sendPost(taskStructure);
     dispatch(post(taskStructure));
     setTask("");
   }, [categoryOption, task, urgentLevel]);
@@ -30,6 +36,7 @@ export default function Home() {
 
       <AddTask
         task={task}
+        setTitle={setTitle}
         setUrgentLevel={setUrgentLevel}
         setTask={setTask}
         setCategoryOption={setCategoryOption}
