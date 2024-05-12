fb105fc36359af2d522528525c767c218c6cf540
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-11 20:06:41 -0700

    [addTask] updated ui and css for addTask to edit and delete task

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -2,6 +2,8 @@ import { Dispatch, SetStateAction } from "react";
 import { Uregentlevel, Categories } from "../lib/hardData";
 import { posts } from "../redux/features/postsSlice";
 import { useAppSelector } from "../redux/hooks";
+import { EditIcon } from "./svg/EditIcon";
+import { DeleteIcon } from "./svg/DeleteIcon";
 
 interface AddTaskTypes {
   task: string;
@@ -21,67 +23,88 @@ export default function AddTask({
   addTask,
 }: AddTaskTypes) {
   const postsList = useAppSelector(posts);
+
+  console.log("post", postsList);
   return (
-    <div className="flex justify-center items-start">
-      <div>
-        <div className="pb-3">
-          <input
-            placeholder="Task Title"
-            onChange={(e) => setTitle(e.target.value)}
-            className="bg-[#1e1e1e] placeholder-[#716a60] rounded-xl mr-4 text-white"
-          />
-          <input
-            placeholder="write your next task"
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-            className="bg-[#1e1e1e] placeholder-[#716a60] rounded-xl text-white"
-          />
+    <div className="">
+      <div className="flex justify-center items-start">
+        <div className="pb-6">
+          <div className="pb-3">
+            <input
+              placeholder="Task Title"
+              onChange={(e) => setTitle(e.target.value)}
+              className="bg-[#1e1e1e] placeholder-[#716a60] rounded-xl mr-4 text-white"
+            />
+            <input
+              placeholder="write your next task"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+              className="bg-[#1e1e1e] placeholder-[#716a60] rounded-xl text-white"
+            />
+          </div>
+
+          <div className="flex justify-center">
+            <select
+              name="urgentlevel"
+              id="urgentlevel"
+              onClick={(event) => setUrgentLevel(event.currentTarget.value)}
+              className="mr-4 rounded-full cursor-pointer font-semibold"
+            >
+              {Uregentlevel.map((level, i) => (
+                <option value={level} key={i}>
+                  {level}
+                </option>
+              ))}
+            </select>
+
+            <select
+              name="category"
+              id="category"
+              onClick={(event) => setCategoryOption(event.currentTarget.value)}
+              defaultValue={Categories[0]}
+              className="rounded-full cursor-pointer p-1 font-semibold"
+            >
+              {Categories.map((category, i) => (
+                <option value={category} key={i}>
+                  {category}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
 
-        <div className="flex justify-center">
-          <select
-            name="urgentlevel"
-            id="urgentlevel"
-            onClick={(event) => setUrgentLevel(event.currentTarget.value)}
-            className="mr-4 rounded-full cursor-pointer"
-          >
-            {Uregentlevel.map((level, i) => (
-              <option value={level} key={i}>
-                {level}
-              </option>
-            ))}
-          </select>
-
-          <select
-            name="category"
-            id="category"
-            onClick={(event) => setCategoryOption(event.currentTarget.value)}
-            defaultValue={Categories[0]}
-            className="rounded-full cursor-pointer"
-          >
-            {Categories.map((category, i) => (
-              <option value={category} key={i}>
-                {category}
-              </option>
-            ))}
-          </select>
-        </div>
+        <button
+          aria-label="post todo"
+          type="button"
+          onClick={() => addTask()}
+          className="bg-[#ff5531] px-5 py-[12.5px] rounded-full text-2xl font-extrabold ml-5"
+        >
+          +
+        </button>
       </div>
 
-      <button
-        aria-label="post todo"
-        type="button"
-        onClick={() => addTask()}
-        className="bg-[#ff5531] px-5 py-[13px] rounded-full text-2xl font-extrabold ml-5"
-      >
-        +
-      </button>
-
       {postsList?.map((list, i) => (
-        <div className="flex gap-3" key={i}>
-          <div>{list.post}</div>
-          <div>{list.category}</div>
-          <div>{list.urgentLevel}</div>
+        <div
+          className="flex gap-3 items-center justify-around max-w-xs m-auto rounded-full px-3 py-4 text-white border-2	border-[#514c48] bg-[#1e1e1e]"
+          key={i}
+        >
+          <div className="flex items-center gap-2">
+            <div className="border-2 border-[#db4a2d] h-3 w-3 rounded-full bg-[#1e1e1e]"></div>
+            <div>{list.post}</div>
+            <div>{list.category}</div>
+            <div>{list.urgentLevel}</div>
+          </div>
+
+          <div className="flex gap-2">
+            <EditIcon
+              className="cursor-pointer"
+              onClick={(e) => console.log(e)}
+            />
+            <DeleteIcon
+              className="cursor-pointer"
+              onClick={(e) => console.log(e)}
+            />
+          </div>
         </div>
       ))}
     </div>
