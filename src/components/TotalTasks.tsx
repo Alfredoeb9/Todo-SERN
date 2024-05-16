2e24b94f80953eafec4a272c70cd1e687ee389e7
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-15 19:59:02 -0700

    [clean up]

--- src/components/TotalTasks.tsx
+++ src/components/TotalTasks.tsx
@@ -1,18 +1,8 @@
-import { useEffect, useState } from "react";
 import { useAppSelector } from "../redux/hooks";
 
 export default function TotalTasks() {
   const todos = useAppSelector((state) => state.post.postList);
   const completedTodos = useAppSelector((state) => state.post.completedTodos);
-  const [completedCounter, setCompletedCounter] = useState<number>(0);
-
-  // useEffect(() => {
-  //   todos.map((todo) => {
-  //     if (todo.completed) {
-  //       setCompletedCounter(completedCounter + 1);
-  //     }
-  //   });
-  // }, [todos]);
 
   return (
     <div className="flex justify-center items-center py-9 gap-3 mb-6 max-w-sm m-auto text-white border-2	border-[#514c48] bg-[#1e1e1e]">
