2e904663ebafa86b27cdb03aac91edfacde80b08
Alfredoeb9 <alfredoeb96@gmail.com>
2024-05-17 11:16:41 -0700

    [redux] created logic if user is in local they auto sign in and pull data

--- src/components/Providers/ReduxProvider.tsx
+++ src/components/Providers/ReduxProvider.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { Provider } from "react-redux";
 import { AppStore, store } from "../../redux/store";
+import { login } from "../../redux/features/userSlice";
 
 export default function ReduxProvider({
   children,
@@ -8,8 +9,11 @@ export default function ReduxProvider({
   children: React.ReactNode;
 }) {
   const storeRef = useRef<AppStore | null>(null);
+  const userEmail = JSON.parse(localStorage.getItem("user")!);
   if (!storeRef.current) {
     storeRef.current = store;
+
+    if (userEmail) storeRef.current.dispatch(login(userEmail));
   }
   return <Provider store={storeRef.current}>{children}</Provider>;
 }
